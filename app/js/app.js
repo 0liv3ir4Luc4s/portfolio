87927db156093db616e7019bb5b8b52539d4c032
Lucas Oliveira <0liv3ir4.luc4s@gmail.com>
2021-12-14 21:16:57 -0300

    feat: Add responsivity

--- app/js/app.js
+++ app/js/app.js
@@ -1,6 +1,7 @@
-const project_cards = Array.from(document.querySelector("project-card"));
+const project_cards = Array.from(document.querySelectorAll(".project-card"));
+console.log(project_cards);
 project_cards.forEach((card) =>
-    card.addEventListerner("click", (ev) => {
+    card.addEventListener("click", (ev) => {
         const textSelected = window.getSelection().toString();
         if(!textSelected) {
             card.querySelector("a").click();
